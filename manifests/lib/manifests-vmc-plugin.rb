879f8d506897a20d74241efe727a7ae528a3c584
Alex Suraci <suraci.alex@gmail.com>
2012-07-23 12:01:59 -0700

    manifests: fix saving app memory as bytes
    Change-Id: Ie42720dfbbc70aec309c8d43688943d857504856

--- manifests/lib/manifests-vmc-plugin.rb
+++ manifests/lib/manifests-vmc-plugin.rb
@@ -478,7 +478,7 @@ module VMCManifests
       "name" => app.name,
       "framework" => app.framework.name,
       "runtime" => app.runtime.name,
-      "memory" => human_size(app.memory, 0),
+      "memory" => human_size(app.memory * 1024 * 1024, 0),
       "instances" => app.total_instances,
       "url" => app.url
     }
