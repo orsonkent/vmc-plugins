eaf1905e57ebbbc38236f301174974a6f4a81db2
Ali Moghadam <alighm@gmail.com>
2012-10-30 00:14:15 -0700

    Migrating over mcf commands from vmc to vmc-ng as a vmc-plugin
    Change-Id: Ib17fa407b51115057500521d32a373e094f4d648

---
+++ mcf/config/refresh_ip.rb
@@ -0,0 +1,24 @@
+#!/var/vcap/bosh/bin/ruby
+require 'socket'
+
+A_ROOT_SERVER = '198.41.0.4'
+
+begin
+  retries ||= 0
+  route ||= A_ROOT_SERVER
+  orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true
+  ip_address = UDPSocket.open do |s|
+    s.connect(route, 1); s.addr.last
+  end
+rescue Errno::ENETUNREACH
+# happens on boot when dhcp hasn't completed when we get here
+  sleep 3
+  retries += 1
+  retry if retries < 10
+ensure
+  Socket.do_not_reverse_lookup = orig
+end
+
+File.open("/tmp/ip.txt", 'w') do |file|
+  file.write(ip_address)
+end
