c8239eb9d661bef0fb46710c0fbc74bc9137332b
Joe <joe@appfog.com>
2013-03-06 14:31:36 -0800

    legacy mem command

---
+++ appfog/lib/appfog-vmc-plugin/deprecated/mem.rb
@@ -0,0 +1,40 @@
+module VMC::App
+  class Mem < Base
+    IS_UTF8 = !!(ENV["LC_ALL"] || ENV["LC_CTYPE"] || ENV["LANG"] || "")["UTF-8"].freeze
+
+    desc "Show app memory usage"
+    group :apps
+    input :app, :desc => "App to show", :argument => :required,
+          :from_given => by_name(:app)
+    input :mem, :desc => "Memory limit", :argument => :optional
+
+    def mem
+      app = input[:app]
+
+      if input.has?(:mem)
+        mem = input[:mem]
+      else
+        mem = input[:mem, app.memory]
+      end
+
+      app.memory = megabytes(mem) if input.has?(:mem)
+      fail "No changes!" unless app.changed?
+
+      with_progress("Scaling #{c(app.name, :name)}") do
+        app.update!
+      end
+
+      if app.started?
+        invoke :restart, :app => app
+      end
+
+    end
+
+    private
+
+    def ask_mem(default)
+      ask("Memory Limit", :choices => memory_choices(default),
+          :default => human_mb(default), :allow_other => true)
+    end
+  end
+end
